2b478923ae80a4725df33f6a76050150ea2c7c47
smruthi19 <49352031+smruthi19@users.noreply.github.com>
2021-07-05 00:10:29 -0400

    Update addedjsother.js

--- newstoday/assets/js/addedjsother.js
+++ newstoday/assets/js/addedjsother.js
@@ -220,7 +220,8 @@ function loadhomenews(input){
 
 
     var urlfirsthalf="https://gnews.io/api/v4/top-headlines?";
-    var urlsecondhalf="category=" +input+ "&token=c3d778f78051de561ad0e0d91793aee8";
+    var urlsecondhalf="q=" +input+ "&token=b0a8f144537fb8306f5e7dd46880da8d";
+
   
   
              
